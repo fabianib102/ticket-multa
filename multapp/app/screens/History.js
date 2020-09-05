3d99f6b8ce9534762d2f12098b1fbf266247145a
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2020-09-04 21:48:43 -0300

    empezando a mostrar el listado de multas

--- multapp/app/screens/History.js
+++ multapp/app/screens/History.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import Multa from "../components/Multa";
 
-export default function History (){
+const History = () => {
+    const renderMulta = itemData => {
+        return (
+            <Multa
+                id={itemData.item.id}
+                dni={itemData.item.dni}
+                nombre={itemData.item.nombre}
+                extracto={itemData.item.extracto}
+            />
+        );
+    }
+
     return(
-        <View>
-            <Text>Es history de la app</Text>
+        <View style={estilos.screen}>
+            <FlatList
+                keyExtractor={(item, index) => item.id}
+                data={[{id: "1", dni: "12345678", nombre: "pepe", extracto: "coso"}]}
+                renderItem={renderMulta}
+                style={estilos.list}
+            />
         </View>
     );
 }
+
+const estilos = StyleSheet.create({
+    screen: {
+        flex: 1,
+    },
+    list: {
+        paddingHorizontal: 5,
+    }
+});
+
+export default History;
