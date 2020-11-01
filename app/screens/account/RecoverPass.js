049f9121f50cef58bbb34600958625b042d0f752
Beto-23 <mansilla_alberto@outlook.com>
2020-10-31 22:17:19 -0300

    Reset Password

--- app/screens/account/RecoverPass.js
+++ app/screens/account/RecoverPass.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import {StyleSheet, View, Text, Image, ScrollView } from 'react-native';
 import {KeyboardAwareScrollView} from "react-native-keyboard-aware-scroll-view";
 import {Input, Icon, Button} from 'react-native-elements';
+import firebase from '../../utils/firebase';
+import {isEmpty} from 'lodash';
+import {validateEmail} from "../../utils/validation";
+import Toast from "react-native-easy-toast";
 
-export default function RecoverPass({ navigation }) {
+export default function RecoverPass(props) {
+    const [formData, setFormData] = useState(defaultFormValue());
+    const [loading, setLoading] = useState(false);
+    const toastRef = useRef();
+
+    const recuperarPass = () => {
+        if(isEmpty(formData.email)){
+            toastRef.current.show("El campo Email es obligatorios")
+        }else{
+            if(!validateEmail(formData.email)){
+                toastRef.current.show("El email no es correcto")
+            }else{
+                    setLoading(true);
+                    firebase.auth().sendPasswordResetEmail(formData.email).then(function() {
+                        setLoading(false);
+                        toastRef.current.show("Se ha enviado un mail a la dirección indicada")
+                        console.log('EMAIL ENVIADO A ' + formData.email);
+                    }).catch(function(error) {
+                        setLoading(false);
+                        console.log(error);
+                        toastRef.current.show("Ha ocurrido un error, intente nuevamente")
+                    });
+                }
+            }
+    }
+
+    const onChange = (e, type) => {
+        setFormData({...formData, [type]: e.nativeEvent.text})
+    }
+
     return (
         <KeyboardAwareScrollView>
             <Image
@@ -16,14 +49,23 @@ export default function RecoverPass({ navigation }) {
                 <Input
                     placeholder="Email"
                     containerStyle={styles.inputForm}
+                    onChange={(e)=>onChange(e, "email")}
                 />
                 <Button
                     title="Restablece"
                     style={styles.btnRecover}
+                    onPress={recuperarPass}
                 />
             </View>
+            <Toast ref={toastRef} position="center" opacity={0.9} />
         </KeyboardAwareScrollView>
     );
+}
+
+function defaultFormValue(){
+    return{
+        email: ""
+    }
 }
 
 const styles = StyleSheet.create({
