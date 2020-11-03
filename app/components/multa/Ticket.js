5158279a650b6eb8bb01e2f55c356238d296b3c3
Beto-23 <mansilla_alberto@outlook.com>
2020-11-03 06:39:48 -0300

    Ticket First Instance Template

---
+++ app/components/multa/Ticket.js
@@ -0,0 +1,31 @@
+const template = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ticket</title>
+    <style>
+        h1 {color:red;}
+        p {color:blue;}
+        table, th, td {
+            border: 1px solid black;
+        }
+      </style>
+</head>
+<body>
+    <table>
+        <th>
+            <tr>
+                <td>
+                    <img src="https://i.imgur.com/SUw60cR.png" alt="Multapp-Logo">
+                </td>
+                <td>
+                    Hello from my HTML.. Multapp (?
+                </td>
+            </tr>
+        </th>
+    </table>
+</body>
+</html>`;
+
+export default template;
