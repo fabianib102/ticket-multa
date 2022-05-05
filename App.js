ce2678f7897f4b8ba0fb6c22ce68a377fd065aef
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2022-05-05 20:21:54 -0300

    adding theme

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { LogBox } from 'react-native';
+import { ThemeProvider } from 'react-native-elements';
 import Navig from './app/navigations/Navigation';
 
 function App() {
@@ -11,9 +12,39 @@ function App() {
         "Invalid prop `color` supplied to `Text`",
         "Animated: 'useNativeDriver'"
     ]);
-    
+
+    const theme = {
+        CheckBox: {
+            containerStyle: {
+                borderColor: 'rgba(0, 0, 0, 0)',
+                marginLeft: 0,
+                marginRight: 0,
+                paddingHorizontal: 0,
+                backgroundColor: 'rgba(0, 0, 0, 0)'
+            },
+            textStyle: {
+                fontSize: 16,
+                fontWeight: 'normal'
+            }
+        },
+        Input: {
+            containerStyle: {
+                marginBottom: 16,
+                borderWidth: 1,
+                borderColor: '#c4c4c4',
+                borderRadius: 4,
+                backgroundColor: 'white'
+            },
+            inputContainerStyle: {
+                borderBottomColor: 'rgba(0, 0, 0, 0)'
+            },
+        }
+    };
+
     return (
-        <Navig/>
+        <ThemeProvider theme={theme}>
+            <Navig />
+        </ThemeProvider>
     )
 }
 
