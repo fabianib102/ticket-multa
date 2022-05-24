3c119871c939c84f6673dd82063102f70f79718b
Emmanuel Garcia <emmanuelgarcia1997@outlook.com.ar>
2022-05-24 20:57:34 -0300

    removing unused imports

--- app/components/Modal.js
+++ app/components/Modal.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {StyleSheet, Text, View} from 'react-native';
-import { Button, Overlay } from 'react-native-elements';
+import { StyleSheet } from 'react-native';
+import { Overlay } from 'react-native-elements';
 
 export default function Modal(props){
 
